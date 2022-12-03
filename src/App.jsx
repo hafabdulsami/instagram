c3d67d1a9abee8cada1e2561f86f5e48d11108b9
Hamza Ahmed <ahmedhamza884@github.com>
2022-12-03 17:47:42 +0500

    Login form setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Header from './login-page/component/header/Header'
-import Login from './login-page/component/loginn/Loginn'
+import React from "react";
+import Header from "./login-page/component/header/Header";
+import Login from "./login-page/component/loginn/Loginn";
 
 const App = () => {
-    return (
-        <>
-            <Header/>
-            <Login mess1 = "Phone number,username, or email" mess2 ="Password"/>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <Login />
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/login-page/component/loginn/Loginn.jsx
+++ src/login-page/component/loginn/Loginn.jsx
@@ -1,21 +1,79 @@
-import React from 'react'
-import './loginn.css'
-import {BsEye} from 'react-icons/bs' 
+import React, { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import "./loginn.css";
 
-const loginn = (props) => {
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = (e) => {};
+
+  const handleFieldChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
-    <loginn>
-      <div className="login">
-        <div className='input email'>
-          <input type="text" placeholder= {props.mess1} autoComplete ="off"/>
-        </div>
-        <div className='input password'>
-          <input type="text" className='field' placeholder= {props.mess2} autoComplete ="off"/>
-          <button className='btn'><BsEye color='white' /></button>
-        </div>
-      </div>
-    </loginn>
-  )
-}
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          {<Form.Label>Email address</Form.Label>}
+          <Form.Control
+            placeholder={"Email address"}
+            value={formData.email}
+            name="email"
+            onChange={handleFieldChange}
+            type="email"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          {<Form.Label>Password</Form.Label>}
+          <Form.Control
+            placeholder={"Password"}
+            value={formData.password}
+            name="password"
+            onChange={handleFieldChange}
+            type="password"
+            required
+          />
+        </Form.Group>{" "}
+        {/* <PhoneInput
+            className="mb-3 custom-input"
+            country={'us'}
+            value={phoneValue}
+            onChange={phone => setPhoneValue(phone)}
+          /> */}
+        <Row className="justify-content-between align-items-center mb-2">
+          <Col xs="auto">
+            <Form.Check type="checkbox" id="rememberMe" className="mb-0">
+              <Form.Check.Input
+                type="checkbox"
+                name="remember"
+                checked={formData.remember}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    remember: e.target.checked,
+                  })
+                }
+              />
+              <Form.Check.Label className="mb-0 text-700">
+                Remember me
+              </Form.Check.Label>
+            </Form.Check>
+          </Col>
+        </Row>
+        <Form.Group className="mb-4">
+          <Button variant="primary" type="submit" className="px-4 mx-0">
+            Save
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
 
-export default loginn
+export default Login;
