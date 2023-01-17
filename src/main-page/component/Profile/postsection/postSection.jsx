c8ebdfac5b4ba6bcf0beb427fbfe0e6fe99d5068
hafabdulsami <abdulsami699@gmail.com>
2023-01-17 11:52:43 -0800

    animation to like button is added

--- src/main-page/component/Profile/postsection/postSection.jsx
+++ src/main-page/component/Profile/postsection/postSection.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './postsection.css'
-import { AiOutlineHeart } from 'react-icons/ai'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 import { FaRegComment } from 'react-icons/fa'
 import { FiSend } from 'react-icons/fi'
 import { FaRegBookmark } from 'react-icons/fa'
 import { BsThreeDots } from "react-icons/bs"
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 import testdata from '../../../assest/testdata.png'
 import testdata1 from '../../../assest/wp5592301.jpg'
 
@@ -25,12 +29,30 @@ const postSection = (props) => {
                 <div className="lower-part">
                     <div className="action-part">
                         <div className="left-actionpart">
-                            <div className="like"><AiOutlineHeart /></div>
-                            <div className="comment"><FaRegComment /></div>
-                            <div className="send"><FiSend /></div>
+                            <div className="spac"></div>
+                            <div className="like">
+                                <FormControlLabel
+                                    control={<Checkbox icon={<AiOutlineHeart color='white' size={25} />}
+                                        checkedIcon={<AiFillHeart size={25} />}
+                                        name="checkedH" size='medium' style={{ backgroundColor: 'transparent' }} />}
+                                /></div>
+                            <div className="comment">
+                                <FormControlLabel
+                                    control={<Checkbox icon={<FaRegComment color='white' size={20} />}
+                                        checkedIcon={<FaRegComment color='white' size={20} />}
+                                        size='medium' style={{ backgroundColor: 'transparent' }} disableRipple />}
+                                />
+                            </div>
+                            <div className="send">
+                                <FormControlLabel
+                                    control={<Checkbox icon={<FiSend color='white' size={20} />}
+                                        checkedIcon={<FiSend color='white' size={20} />}
+                                        size='medium' style={{ backgroundColor: 'transparent' }} disableRipple />}
+                                /></div>
                         </div>
                         <div className="right-actionpart">
-                            <div className="save"><FaRegBookmark /></div>
+                            <div className="save">
+                                <FaRegBookmark size={20} /></div>
                         </div>
                     </div>
                     <div className="extra">
